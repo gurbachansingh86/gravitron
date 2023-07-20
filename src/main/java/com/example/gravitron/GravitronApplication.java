c625d2173b06979e591f83c15c2201d66ecb4f4d
Gurbachan Singh <gurbachan.singh@getfareye.com>
2023-07-20 19:49:01 +0530

    set work flow

---
+++ src/main/java/com/example/gravitron/GravitronApplication.java
@@ -0,0 +1,13 @@
+package com.example.gravitron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravitronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GravitronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gravitron/GravitronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gravitron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravitronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
